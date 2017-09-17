2295e1810dde188962f1fe960dd0ceeaea6d7a5c
samsonf21@gmail.com <samsonf21@gmail.com>
2017-09-17 18:09:17 +0100

    second commit

--- DiscordChatBot/DiscordBot.py
+++ DiscordChatBot/DiscordBot.py
@@ -7,4 +7,12 @@ import os
 client -Discord.client()
 
 client.event
-async def
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('-------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!isYouCool')
